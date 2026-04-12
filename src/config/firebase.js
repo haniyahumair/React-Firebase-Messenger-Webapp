4fe06a3fe5970a286ea819e24d40bf0ef8e30cf3
Haniyah Umair <haniyah.umair.18@gmail.com>
2026-04-12 17:00:45 +0300

    Initial Commit

---
+++ src/config/firebase.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAFpRr0DNMCWMpbyYLuOrj_HM1eyRrwOWo",
+  authDomain: "react-basic-messenger.firebaseapp.com",
+  projectId: "react-basic-messenger",
+  storageBucket: "react-basic-messenger.firebasestorage.app",
+  messagingSenderId: "867905706284",
+  appId: "1:867905706284:web:61e638a5a3e82b8893b489",
+  measurementId: "G-GPGX9SHQFY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
